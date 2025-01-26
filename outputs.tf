e8bd045d745df9a2f10be4ca9e0c9158ba749277
Geoff Hickman <geoff.hickmanii@gmail.com>
2025-01-26 16:30:42 -0600

    update output of s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 
 
 # ./cicd.tf
-output "s3_bucket_id" {
-  description = "The S3 bucket ID"
-  value       = aws_s3_bucket.artifact_bucket.id
+output "s3_bucket_artifact_id" {
+  description = "The S3 bucket ID to store artifacts for the application"
+  value       = join("", aws_s3_bucket.artifact_bucket.*.id) != null ? aws_s3_bucket.artifact_bucket.id : var.byo_s3_bucket_artifact_id
 }
 
 /*
