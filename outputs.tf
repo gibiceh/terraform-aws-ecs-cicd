504732ac9b7b421e2dcdeec8c580bd443e502f15
Geoff Hickman <geoff.hickmanii@gmail.com>
2025-01-26 17:14:40 -0600

    provide method to include a s3 bucket created outside of the module for artifacts

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 # ./cicd.tf
 output "s3_bucket_artifact_id" {
   description = "The S3 bucket ID to store artifacts for the application"
-  value       = join("", aws_s3_bucket.artifact_bucket.*.id) != null ? aws_s3_bucket.artifact_bucket.*.id : var.byo_s3_bucket_artifact_id
+  value       = join("", aws_s3_bucket.artifact_bucket.*.id) != null ? join("", aws_s3_bucket.artifact_bucket.*.id) : var.byo_s3_bucket_artifact_id
 }
 
 /*
