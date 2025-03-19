7476610372e7d62fd63e9266d851d80b13065dd6
Geoff Hickman <geoff.hickmanii@gmail.com>
2025-03-19 12:15:37 -0500

    update s3 sse

--- main.tf
+++ main.tf
@@ -27,8 +27,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse_artifact_b
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "aws/s3"
+      sse_algorithm = "AES256"
     }
   }
 }
